0a64e5f4885d8e05e581a12eee21e321de98517c
Win94js <winthuaung6996@gmail.com>
2026-09-17 11:58:01 +0630

    Adding New Project Coffee Card App

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const  Text("Coffee Card App" ,
+          style: TextStyle(
+            color: Colors.white,
+          ),),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.orange,
+        width: 200,
+        height: 100,
+        padding: EdgeInsets.all(20.0),
+        margin: const EdgeInsets.only(left: 20),
+        child: Text("Hello Alpha Aquarium", style: TextStyle(
+          fontSize: 18,
+          letterSpacing: 4,
+          decoration: TextDecoration.underline,
+          fontStyle: FontStyle.italic,
+
+        ),),
+      )
+    );
+  }
+}
